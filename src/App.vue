3521c8c2e26656c659ac31b8908bbcc9496f8678
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-03-16 17:34:23 -0300

    desafio ok

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+﻿<template>
+    <div id="app">
+        <h1>Crear una nueva tarea</h1>
+        Tarea: <input type="text" id="inputTarea" />
+        <button @click="agregarTarea">Crear</button>
+
+        <ul>
+            <li v-for="(tarea,index) in listaTareas">
+                <Hijo :tarea="tarea" :index="index" @eliminarTareaHijo="eliminarTareaPadre"></Hijo>
+            </li>
+        </ul>
+
+        
+    </div>
+</template>
+<script>
+    import Hijo from './components/Hijo.vue';   
+
+    export default {
+        components: { Hijo },
+        data() {
+                return {
+                    listaTareas: [],
+                    tarea : ""
+		        }
+        },
+        methods: {
+            agregarTarea() {
+                this.tarea = $("#inputTarea").val();
+                this.listaTareas.push(this.tarea);//se agrega tarea
+            },
+            eliminarTareaPadre(index) {
+                console.log("llega al padre el index-- " + index);
+                this.listaTareas.splice(index,1); // se elimina tarea
+
+            }
+        }
+    };
+</script>
+<style>
+    #app {
+        font-size: 25px;
+        font-family: 'Calibri', sans-serif;
+        color: darkblue;
+        background: lightblue;
+    }
+</style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="hellovue">
+       {{index+1}} - {{tarea}}<button class="hijo__boton" @click="eliminarTarea(index)">Eliminar</button>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'hijo',
+        props: {
+            tarea: String,
+            index: Number
+        },
+        methods: {
+            eliminarTarea(index) {
+                console.log("el index que voy a enviar es-- " + index);
+                this.$emit('eliminarTareaHijo', index); //se envía a papá
+            }
+        }
+    };
+</script>
+<style scoped>
+    .hellovue {
+        display: inline;
+    }
+
+    .helloimg {
+        width: 25%;
+        height: 25%;
+    }
+    .hijo__boton{
+        margin-left: 10px;
+    }
+</style>
